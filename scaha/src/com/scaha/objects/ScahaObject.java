fa7d74dc9ba009929aef633da8c123f1f6820570
dbigelow25 <dbigelow@the-rinks.com>
2014-01-10 12:05:32 -0800

    Testing DB Connection
    Working to test the database connections through JSP

---
+++ scaha/src/com/scaha/objects/ScahaObject.java
@@ -0,0 +1,12 @@
+package com.scaha.objects;
+
+/**
+ * This is the base object of which all scaha objects will be modeled after
+ * it will have all the required methods that need to be subscribed to when building an object
+ * 
+ * @author dbigelow
+ *
+ */
+public abstract class ScahaObject {
+
+}
