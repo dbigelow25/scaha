ffa5ffe15c92e315c3c63ef1c6e1e4fa125c0b1c
dbigelow25 <dbigelow@the-rinks.com>
2014-01-16 15:08:26 -0800

    Login Logout
    Bean States using login logout and texting db connection

--- scaha/src/com/scaha/beans/LoginBean.java
+++ scaha/src/com/scaha/beans/LoginBean.java
@@ -1,5 +1,14 @@
 package com.scaha.beans;
 
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import com.gbli.connectors.ScahaDatabase;
+import com.gbli.context.ContextManager;
+
 /**
  * LoginBean.java
  * 
@@ -7,9 +16,10 @@ package com.scaha.beans;
 
 public class LoginBean
 {
-    private String name;
-    private String password;
-
+    private String name = null;
+    private String password = null;
+	private Boolean isAuth = false;
+	private String origin = null;
 
     public String getName ()
     {
@@ -33,4 +43,70 @@ public class LoginBean
     {
         this.password = password;
     }
+    
+    public void login() {
+
+    	ContextManager.getLoggerContext();
+    	ScahaDatabase mydb = (ScahaDatabase) ContextManager.getDatabase("ScahaDatabase"); 
+    	
+    	mydb.free();
+    	
+    	// pull profile into the Login Bean..
+    	
+
+    	try {
+    		if (name.equals("dave")) {
+    			this.isAuth = true;
+    			if (origin != null) {
+        			FacesContext context = FacesContext.getCurrentInstance();
+        			FacesContext.getCurrentInstance().getExternalContext().redirect(origin);
+//    				origin = ((HttpServletRequest)context.getExternalContext().getRequest()).getRequestURL().toString();
+    			}
+    			
+    		} else {
+    			FacesContext.getCurrentInstance().addMessage(
+                        null,
+                        new FacesMessage(FacesMessage.SEVERITY_WARN,
+                        "Invalid Login!",
+                        "Please Try Again!"));
+
+    			name = null;
+    			password = null;
+    					
+    		}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 
+    }
+    
+    
+    public void verifyUserLogin(){
+		FacesContext context = FacesContext.getCurrentInstance();
+		try{
+			if(!isAuth){
+				this.origin = ((HttpServletRequest)context.getExternalContext().getRequest()).getRequestURL().toString();
+				context.getExternalContext().redirect("Welcome.xhtml");
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+    
+    public String logout() {
+    	this.name = null;
+    	this.password = null;
+    	this.isAuth = false;
+    	this.origin = null;
+        try {
+			FacesContext.getCurrentInstance().getExternalContext().redirect("Welcome.xhtml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+        return "login";
+     }
 }
