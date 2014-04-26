a99ef3be35ddcd81d5b8bdc84032e5b09b60ada3
David Bigelow <dbigelow@the-rinks.com>
2014-04-25 18:47:30 -0700

    minor change
    import section chabge

--- scaha/src/com/scaha/beans/MailTreeBean.java
+++ scaha/src/com/scaha/beans/MailTreeBean.java
@@ -8,8 +8,8 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;  
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;  
-import javax.faces.view.ViewScoped;
 import org.primefaces.model.TreeNode;  
 import org.primefaces.model.CheckboxTreeNode;  
 
