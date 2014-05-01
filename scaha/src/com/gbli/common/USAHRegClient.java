3eed6e7af3dca1d093d0abc818bce163b2fca7ad
David Bigelow <dbigelow@the-rinks.com>
2014-05-01 05:13:13 -0700

    USA Hockey Testing
    Testing interface

--- scaha/src/com/gbli/common/USAHRegClient.java
+++ scaha/src/com/gbli/common/USAHRegClient.java
@@ -27,7 +27,7 @@ public class USAHRegClient {
 		Simple3Des crypt = new Simple3Des();
  
 		System.out.println("START:" + crypt.encryptText(_strUSAH));
-		
+		LOGGER.info("http://scahalookup.azurewebsites.net/ReqUSAH.aspx?usah='" + crypt.encryptText(_strUSAH).replace("+", ":PLUS:") + "'");
 		URL url = new URL("http://scahalookup.azurewebsites.net/ReqUSAH.aspx?usah='" + crypt.encryptText(_strUSAH).replace("+", ":PLUS:") + "'");
 		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 		String inputLine = null;
