c489ce426a7e78fabf2dedf7720bee72cc44bf4d
David Bigelow <dbigelow@the-rinks.com>
2014-05-05 04:05:49 -0700

    Menu Width and DBTest on Admin Panel
    Fixing menu Width and DBTest on Admin Panel

--- scaha/src/com/scaha/beans/ScahaBean.java
+++ scaha/src/com/scaha/beans/ScahaBean.java
@@ -99,6 +99,23 @@ public class ScahaBean implements Serializable,  MailableObject {
 		db.free();
 	}
 
+	/**
+	 * Wait some seconds before freeing up the connection
+	 * @param _isec
+	 */
+	public void testDB(int _isec) {
+		ScahaDatabase db = (ScahaDatabase) ContextManager.getDatabase("ScahaDatabase");
+		
+		try {
+		    Thread.sleep(1000*_isec);
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+		
+		db.free();
+		
+		
+	}
 	@SuppressWarnings("unchecked")
 	private void resetClubLists() {
 		if (this.ScahaClubList != null) {
