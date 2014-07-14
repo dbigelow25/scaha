ad5a56d888ed42f5b02a2b059c3ce1a7fbf9a16c
David Bigelow <dbigelow@the-rinks.com>
2014-07-13 22:06:29 -0700

    sync Participants and Games
    Logic  to sync participants and games to a given set of schedules for a
    given seaons

--- scaha/src/com/gbli/connectors/ScahaDatabase.java
+++ scaha/src/com/gbli/connectors/ScahaDatabase.java
@@ -12,11 +12,16 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Vector;
 import java.util.logging.Logger;
+
+import com.gbli.common.ReturnDataResultSet;
+import com.gbli.common.ReturnDataRow;
 import com.gbli.context.ContextManager;
 import com.scaha.objects.Club;
 import com.scaha.objects.GeneralSeason;
 import com.scaha.objects.Person;
 import com.scaha.objects.Profile;
+import com.scaha.objects.Schedule;
+
 import java.sql.PreparedStatement;
 
 import javax.mail.internet.InternetAddress;
@@ -267,6 +272,154 @@ public class ScahaDatabase extends Database {
 		return tmp;
 	}
 	
-}
+	
+	/**
+	 * This is a DB manipulation Method that syncs all the Teams to a Certain Schedule
+	 * Based upon what they have declared.. 
+	 * 
+	 * @param _sc
+	 */
+	public void syncTeamsToSchedule (Schedule _sc, GeneralSeason _gs) throws SQLException {
+		
+		LOGGER.info("Searching for new teams and defunct teams for Schedule " + _sc);
+		
+		PreparedStatement ps = this.prepareStatement("call scaha.genParticipantChanges(?,?,?)");
+		CallableStatement csAdd = this.prepareCall("call scaha.addParticipantToSchedule(?,?)");
+		CallableStatement csRemove = this.prepareCall("call scaha.removeParticipantFromSchedule(?,?)");
+		ps.setInt(1, _sc.ID);
+		ps.setString(2,_sc.getTag());
+		ps.setString(3,_gs.getTag());
+		ResultSet rs = ps.executeQuery();
+		
+		ReturnDataResultSet rdrs = ReturnDataResultSet.NewReturnDataResultSetFactory(rs);
+		rs.close();
 
+		for (ReturnDataRow row : rdrs) {
+			String sCode = (String)row.elementAt(0);
+			int idTeam = Integer.parseInt(row.elementAt(1).toString());
+			String sName = (String)row.elementAt(2);
+			LOGGER.info("Hello There:" + sCode + ":" + idTeam + ":" + sName);
+			if (sCode.equals("D")) {
+				LOGGER.info("Found a defunct team id[" + idTeam + "] cleaning up for schedule " + _sc);
+				csRemove.setInt(1,idTeam);
+				csRemove.setInt(2,_sc.ID);
+				csRemove.execute();
+			} else if (sCode.equals("A")) {
+				LOGGER.info("Adding Team [" + sName + "] to the participants for schedule: " + _sc.getTag());
+				csAdd.setInt(1,idTeam);
+				csAdd.setInt(2,_sc.ID);
+				csAdd.execute();
+			}
+		}
+		
+		ps.close();
+		csAdd.close();
+		csRemove.close();
+		this.cleanup();
+		
+		LOGGER.info("sychTeamSeeding: Reviewing rankings to ensure they are sequential for Schedule: " + _sc);
+		//
+		// We just keep reexecuting this until we are good.
+		//
+		PreparedStatement ps1 = this.prepareStatement("call scaha.getParticipantKeysAndRankBySchedule(?)");
+		CallableStatement csFixGap = this.prepareCall("call scaha.updateParticipantRankingForSchedule(?,?,?,?)");
+		ps1.setInt(1,_sc.ID);
+		
+		boolean bwork = true;
+		while (bwork) {
+			bwork = false;
+
+			int itargetrank = 0;
+			int irank = 1;
+			int ioldrank = 0;
+			int ikey = 0;
+			
+			//
+			// rerun sql..
+			//
+			rs = ps1.executeQuery();
+			while (rs.next()) {
+				ioldrank = rs.getInt(2);
+				if (irank != ioldrank && itargetrank == 0 ) {
+					itargetrank = irank;
+					LOGGER.info("syncTeamSeeding: Found a ranking gap in Schedule " + _sc  + ". " + rs.getInt(1) + ":" + rs.getInt(2) + ": should be:" + irank);
+					bwork = true;
+				}
+				ikey = rs.getInt(1);
+				irank++;
+			}	
+			rs.close();
+
+			//
+			// If there is work to do.. then we do it.
+			//
+			if (bwork) {
+				LOGGER.info("Re-Gapping Participate " + ikey + "] from rank [" + ioldrank + ":" + itargetrank + "] for schedule: " + _sc);
+				csFixGap.setInt(1,ikey);
+				csFixGap.setInt(2,_sc.ID);
+				csFixGap.setInt(3,ioldrank);
+				csFixGap.setInt(4,itargetrank);
+				csFixGap.execute();
+			}
+		}
+		
+		ps1.close();
+		csFixGap.close();
+
+		CallableStatement csTcs = this.prepareCall("call scaha.updateScheduleTeamCounts(?)");
+		csTcs.setInt(1,_sc.ID);
+		csTcs.execute();
+		csTcs.close();
+		this.cleanup();
+		
+	}
+	
+	
+	
+	/**
+	 * genGames
+	 * 
+	 * This will take a look at the season.. how many teams.. and how many games they have to play.. and generate 
+	 * enough games to cover the season through iteration...
+	 * 
+	 * if the iteration of games are smaller than the number of teams based upon the round robin matrix
+	 * then we generate another iteration.. this makes sure we have enough games to pull from to piece the matchups 
+	 * together.
+	 * 
+	 * We always want more games to choose from then possible games for the weekend.
+	 * @param _se
+	 */
+	public void genGames(Schedule _sc) throws SQLException {
+		
+		LOGGER.info("genGames: Starting to Gen games for Schedule "  + _sc);
+		CallableStatement csDelete = this.prepareCall("call scaha.removeGamesByScheduleIteration(?,?)");
+		CallableStatement csInsert = this.prepareCall("call scaha.insertGamesByScheduleIteration(?,?)");
+		
+		// We will always have at least one iteration.
+		int iCount = 0;
+		int iGames = _sc.getMingamecnt();
+		int iGamesPerIteration = _sc.getTeamcount() - 1 ;
+		
+		LOGGER.info("genGames: Check for " + _sc + ". iCount=" + iCount + ": iGames=" + _sc.getMingamecnt() + ": Team Count-1=" + (_sc.getTeamcount() - 1));
+			
+		//
+		// Lets calculate the number of iterations now..
+		//
+		while (iGames > (iGamesPerIteration*iCount)) {
+			iCount++;
+			int i=1;
+			csInsert.setInt(i++, _sc.ID);
+			csInsert.setInt(i++, iCount);
+			csInsert.execute();
+			LOGGER.info("genGames: Inserted missing games for iteration :" + iCount + ": for schedule "  + _sc);
+		}
+		
+		csDelete.close();
+		csInsert.close();
+		
+		LOGGER.info("genGames: Inserted missing games finished for season " + _sc);
+						
+	}	
+}
+	
 
