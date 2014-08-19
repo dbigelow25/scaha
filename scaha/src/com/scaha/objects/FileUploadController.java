715759021d55086ecc3a2e671351fd8be69fdd4a
David Bigelow <dbigelow@the-rinks.com>
2014-08-18 18:05:54 -0700

    fixed absolute path
    gave wrong description of file path

--- scaha/src/com/scaha/objects/FileUploadController.java
+++ scaha/src/com/scaha/objects/FileUploadController.java
@@ -37,8 +37,7 @@ public class FileUploadController extends ScahaObject implements Serializable {
         scoresheet.setFiledisplayname(prefix + "." + suffix);
         
         try {
-        	String sAbsolutePath = ContextManager.getRealPath();
-        	String destPath = "\\var\\scaha\\scoresheets\\" + scoresheet.getGametype() + "_" + scoresheet.getIdgame() + prefix + "." + suffix;
+        	String destPath = "/var/scaha/scoresheets/" + scoresheet.getGametype() + "_" + scoresheet.getIdgame() + prefix + "." + suffix;
         	
             File destFile = new File(destPath);
         	stream = event.getFile().getInputstream();
